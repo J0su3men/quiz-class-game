374c418a1b786fb987bbcd88c498cef3bd2015b1
mmr0d0lf0333 <r0d0lf0333@hotmail.com>
2017-07-05 12:37:11 -0500

    Add files via upload
    Novedades version 3.1.0:
    *En todos los html ya funciona el menu como el que hicimos el domingo
    *Carrusel listo en maestro.html
    *Login funcionando, hay que detallarlo con un stylesheet.

--- kahoot/php/controldb.php
+++ kahoot/php/controldb.php
@@ -1,37 +1,11 @@
 <?php
-/**
-@author Josue mendez
-* @class ControlDB
-* @brief Modelo de base de datos para la tabla de usuarios
-*Clase para crear la conexión 
-*/
-class ControlDB {
-	
-	function __construct(){
-		# code...
-		try {
-			//Cadena de conexión
-		$this->conexion=mysqli_connect("localhost", "root", "")or die("No se puede conectar a la base de datos");
-		//seleccionar la base de datos
-		mysqli_select_db($this->conexion, "kahoot")or die ("No se ha encontrado la base de datos");
-		//echo "conexion exitosa";
-		} catch (Exception $e) {
-			throw new Exception("Error Processing Request", 1);
-		}	
-	}
-	//funcion consultar y el $sql(consultas) sera el parametro q la funcion pide 
-	function consultar($sql){
-		$res_consulta=mysqli_query($this->conexion, $sql);
-		$datos_array=NULL;
-		while($fila=mysqli_fetch_assoc($res_consulta)) {
-			# code...
-			$datos_array[]=$fila;
-		}
-		return $datos_array;
-	}
-}
+$servidor="localhost";
+$basedatos="kahoot";
+$usuario="root";
+$password="";
 
+$conectar=mysql_connect("$servidor","$usuario","$password")or die(header("Location:index.html?error_login=0"));
+mysql_select_db("$basedatos");
 
-
-
+date_default_timezone_set('America/Mexico_City');
 ?>
--- kahoot/php/valida_sesion.php
+++ kahoot/php/valida_sesion.php
@@ -1,36 +1,24 @@
 <?php
+require("controldb.php");
+
+$nombre=$_POST['usuario'];
+$clave=$_POST['contraseña'];
+
+$consulta="SELECT * FROM tb_user WHERE email_user='$nombre' and pass_user='$clave'";
+$result=mysql_query($consulta, $conectar) or die(mysql_error());
+$num_rows=mysql_num_rows($result);
+
+if($num_rows>0)
+{
+header('Location:http://localhost/kahoot/alumno.html');
+}
+
+else
+{
+	//mysql_query("INSERT INTO registro (nombre,email,usuario,clave) VALUES('".$_POST['nombre']."','".$_POST['correo']."','".$_POST['user']."','".$_POST['pass']."')",$conectar )or die(mysql_error());
 	
-
-
-
-/*$resultados=$obj_connect->consultar("SELECT email_user, pass_user FROM tb_user");
-*/
-
-	$user=$_POST['usuario_sesion'];
-	$password=$_POST['password_sesion'];
-	require "controldb.php";
-
-$obj_connect = new ControlDB();
-	
-	if(isset($_POST['abrir_sesion'])){
-		$valid=false;
-		/*$resultados=$obj_connect->query("SELECT email_user, pass_user FROM tb_user WHERE email_user='$user' and pass_user='$password'");
-		print_r($resultados);*/
-
-		if($resultado=$obj_connect->consultar("SELECT email_user, pass_user FROM tb_user WHERE email_user='$user' and pass_user='$password'")) {
-			$valid = true;
-			
-			}
-			if($valid>0){
-		echo "<script>location.href='http://192.168.1.15/PRACTICAS/CursoPHP/colegio/home.html'</script>";
-		}else {
-			
-			echo '<script>alert("ESTE USUARIO NO EXISTE, PORFAVOR REGISTRESE PARA PODER INGRESAR")</script> ';
-			echo "<script>location.href='http://192.168.1.15/PRACTICAS/CursoPHP/colegio/index.html'</script>";
-			
-			}
-		}
-		
+echo("Error");
+}
 		
 
    
